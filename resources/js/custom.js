5b2cf1d3934972835f47919932c704c3557b2c37
kuangye <ky.kys.cn@hotmail.com>
2023-02-24 16:43:00 +0800

    Update

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,10 +1,1 @@
-$(function () {
-    // to top
-    $(window).on("scroll", function () {
-        let top = document.documentElement.scrollTop || document.body.scrollTop;
-        top > 300 ? $(".k-totop").show(200) : $(".k-totop").hide(100);
-    })
-    $(".k-totop").on("click", function () {
-        $("body,html").animate({scrollTop: 0}, 200, "swing")
-    })
-})
+$(function(){$(window).on("scroll",function(){let o=document.documentElement.scrollTop||document.body.scrollTop;o>300?$(".k-totop").show(200):$(".k-totop").hide(100)});$(".k-totop").on("click",function(){$("body,html").animate({scrollTop:0},200,"swing")})});
